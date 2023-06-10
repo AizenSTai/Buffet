63aa503d2078b7831ee444864e9bdc34902c75ac
AizenSTai <1378tlmh.shirvaniz@gmail.com>
2023-06-11 02:53:39 +0330

    changes

--- src/component/Components/View/Parents/WeeklyChart/ChartDropdown/ChartDropdown.js
+++ src/component/Components/View/Parents/WeeklyChart/ChartDropdown/ChartDropdown.js
@@ -2,36 +2,59 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
-import { Box, Typography, useTheme } from '@mui/material';
+import { Box, Typography, useTheme, Checkbox, FormGroup, FormControlLabel } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import { useTranslation } from 'react-i18next';
 import { useContext } from 'react';
-import {ColorModeContext,tokens} from "../../../../../../theme"
+import { ColorModeContext, tokens } from "../../../../../../theme"
 import { BorderRadius } from 'mdi-material-ui';
+import { useState } from 'react';
+import { useEffect } from 'react';
 export default function ChartDropdown(props) {
-  const { title, icon, icon2, type,left,border } = props
-  const {t,i18n}= useTranslation()
+  const { title, func, itemList, id } = props
+  const { t, i18n } = useTranslation()
+  const [items, setItems] = useState([])
   const theme = useTheme()
   const colors = tokens(theme.palette.mode)
   const colorMode = useContext(ColorModeContext)
+  // useEffect(() => {  
+
+
+  // },[1])
+  function checkSelected(select) {
+    return select.selected == true;
+  }
   return (
-    <PopupState>
+    <PopupState>      
       {(popupState) => (
         <React.Fragment>
           <Box
-            sx={{ color:"blue",pr:2,pl:2,cursor: "pointer",borderRadius:"5px", display: "flex", m:"0 auto",boxSizing:"border-box", }}
+            sx={{ color: "blue", pr: 2, pl: 2, cursor: "pointer", borderRadius: "5px", display: "flex", m: "0 auto", boxSizing: "border-box", }}
             {...bindTrigger(popupState)}
           >
             <Typography>{title}</Typography>
           </Box>
-            <Menu sx={{width:"250px",height:"auto"}} {...bindMenu(popupState)}>
-              <MenuItem onClick={()=>{popupState.close()}}>شیر کم چرب</MenuItem>
-              <MenuItem onClick={()=>{popupState.close()}}>شیر کم چرب</MenuItem>
-              <MenuItem onClick={()=>{popupState.close()}}>شیر کم چرب</MenuItem>
-              <MenuItem onClick={()=>{popupState.close()}}>شیر کم چرب</MenuItem>
-            </Menu>
+          <Menu sx={{ height: "auto", direction: "rtl" }} {...bindMenu(popupState)}>
+            <FormGroup sx={{ width: "auto", pl: 4 }}>
+              {itemList.map((item, index) => {
+                return (
+                  <FormControlLabel control={<Checkbox onChange={() => {
+                    itemList[item.id - 1].selected = true
+                    // console.log(item)
+                    // console.log(items[item.id-1]) 
+                  }} />} label={item.name} />
+                )
+              })}
+            </FormGroup>
+            <Box sx={{ display: "flex", mt: 1 }}>
+              <Button onClick={() => {
+                func(id, itemList.filter(checkSelected))
+                popupState.close()
+              }} sx={{ m: "0 auto", bgcolor: "orange", color: "white", ":hover": { bgcolor: "orange" } }}>انتخاب</Button>
+            </Box>
+          </Menu>
         </React.Fragment>
       )}
     </PopupState>
--- src/component/Components/View/Parents/WeeklyChart/WeeklyChart.js
+++ src/component/Components/View/Parents/WeeklyChart/WeeklyChart.js
@@ -1,15 +1,49 @@
 import { React, useState } from "react";
-import { Box, Typography } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
 import Chartdropdown from "./ChartDropdown/ChartDropdown"
 
 const WeeklyChart = () => {
+    const [sat, setSat] = useState([])
     const [sun, setSun] = useState([])
     const [mon, setMon] = useState([])
     const [tue, setTue] = useState([])
     const [wed, setWed] = useState([])
     const [thu, setThu] = useState([])
+    const [fri, setFri] = useState([])
     const [whichDay, setWhichDay] = useState("")
-
+    const setFoodsHandler = (id, foods) => {
+        switch (id) {
+            case "0":
+                setSat([])
+                // console.log(foods)
+                setSat(foods)
+                break
+            case "1":
+                setSun([])
+                setSun(foods)
+                break
+            case "2":
+                setMon([])
+                setMon(foods)
+                break
+            case "3":
+                setTue([])
+                setTue(foods)
+                break
+            case "4":
+                setWed([])
+                setWed(foods)
+                break
+            case "5":
+                setThu([])
+                setThu(foods)
+                break
+            case "6":
+                setFri([])
+                setFri(foods)
+                break
+            }
+    }
     return (
         <Box sx={{
             boxShadow: '0px 0px 4px #00000001 !important'
@@ -24,27 +58,62 @@ const WeeklyChart = () => {
             <Box sx={{ border: "1px solid black", height: "85%", minHeight: "300px", borderRadius: "10px", display: "flex" }}>
                 <Box sx={{ display: "flex", flexDirection: "column", border: "1px solid black", width: "14%", borderTopLeftRadius: "10px", borderBottomLeftRadius: "10px" }}>
                     <Typography sx={{ borderBottom: "2px solid black", m: 1, textAlign: "center" }}>جمعه</Typography>
+                    <Chartdropdown id="6" title="انتخاب" itemList={[{ name: "دوغ", id: "3" ,selected:false}, { name: "ساندویچ ژامبون", id: "1" ,selected:false}, { name: "مارشمالو", id: "2",selected:false }]} func={setFoodsHandler} />
+                    {fri.map((itm,indx)=>{
+                        return (<Box sx={{p:1,textAlign:"right",pr:2,pl:2}}>{itm.name}</Box>)
+                    })}
+                    {fri.length != 0 && <Button onClick={()=>{setFri([])}}>حذف همه</Button>}
                 </Box>
                 <Box sx={{ display: "flex", flexDirection: "column", border: "1px solid black", width: "14.2%" }}>
                     <Typography sx={{ borderBottom: "2px solid black", m: 1, textAlign: "center" }}>پنجشنبه</Typography>
+                    <Chartdropdown id="5" title="انتخاب" itemList={[{ name: "نون پنیر", id: "1",selected:false }, { name: "پاستیل", id: "2",selected:false }, { name: "چوب شور", id: "3",selected:false }]} func={setFoodsHandler} />
+                    {thu.map((itm,indx)=>{
+                        return (<Box sx={{p:1,textAlign:"right",pr:2,pl:2}}>{itm.name}</Box>)
+                    })}
+                    {thu.length != 0 && <Button onClick={()=>{setThu([])}}>حذف همه</Button>}
                 </Box>
                 <Box sx={{ display: "flex", flexDirection: "column", border: "1px solid black", width: "14.2%", }}>
                     <Typography sx={{ borderBottom: "2px solid black", m: 1, textAlign: "center" }}>چهارشنبه</Typography>
+                    <Chartdropdown id="4" title="انتخاب" itemList={[{ name: "چایی", id: "1",selected:false }, { name: "شیرقهوه", id: "2",selected:false }, { name: "چیپوف", id: "3",selected:false }]} func={setFoodsHandler} />
+                    {wed.map((itm,indx)=>{
+                        return (<Box sx={{p:1,textAlign:"right",pr:2,pl:2}}>{itm.name}</Box>)
+                    })}
+                    {wed.length != 0 && <Button onClick={()=>{setWed([])}}>حذف همه</Button>}
                 </Box>
                 <Box sx={{ display: "flex", flexDirection: "column", border: "1px solid black", width: "14.2%", }}>
                     <Typography sx={{ borderBottom: "2px solid black", m: 1, textAlign: "center" }}>سه شنبه</Typography>
+                    <Chartdropdown id="3" title="انتخاب" itemList={[{ name: "سانویچ گوشت", id: "1",selected:false }, { name: "شیر پرچرب", id: "2",selected:false }, { name: "خلالی", id: "3" ,selected:false}]} func={setFoodsHandler} />
+                    {tue.map((itm,indx)=>{
+                        return (<Box sx={{p:1,textAlign:"right",pr:2,pl:2}}>{itm.name}</Box>)
+                    })}
+                    {tue.length != 0 && <Button onClick={()=>{setTue([])}}>حذف همه</Button>}
                 </Box>
                 <Box sx={{ display: "flex", flexDirection: "column", border: "1px solid black", width: "14.2%", }}>
                     <Typography sx={{ borderBottom: "2px solid black", m: 1, textAlign: "center" }}>دوشنبه</Typography>
+                    <Chartdropdown id="2" title="انتخاب" itemList={[{ name: "های بای", id: "1",selected:false }, { name: "اشترودل", id: "2",selected:false }, { name: "لوبیا", id: "3" ,selected:false}]} func={setFoodsHandler} />
+                    {mon.map((itm,indx)=>{
+                        return (<Box sx={{p:1,textAlign:"right",pr:2,pl:2}}>{itm.name}</Box>)
+                    })}
+                    {mon.length != 0 && <Button onClick={()=>{setMon([])}}>حذف همه</Button>}
                 </Box>
                 <Box sx={{ display: "flex", flexDirection: "column", border: "1px solid black", width: "14.2%", }}>
                     <Typography sx={{ borderBottom: "2px solid black", m: 1, textAlign: "center" }}>یکشنبه</Typography>
+                    <Chartdropdown id="1" title="انتخاب" itemList={[{ name: "نوشابه", id: "1",selected:false }, { name: "شیرکمچرب", id: "2",selected:false }, { name: "ساندویچ مرغ", id: "3" ,selected:false}]} func={setFoodsHandler} />
+                    {sun.map((itm,indx)=>{
+                        return (<Box sx={{p:1,textAlign:"right",pr:2,pl:2}}>{itm.name}</Box>)
+                    })}
+                    {sun.length != 0 && <Button onClick={()=>{setSun([])}}>حذف همه</Button>}
                 </Box>
                 <Box sx={{ display: "flex", flexDirection: "column", border: "1px solid black", width: "14.2%", borderTopRightRadius: "10px", borderBottomRightRadius: "10px" }}>
                     <Typography sx={{ borderBottom: "2px solid black", m: 1, textAlign: "center" }}>شنبه</Typography>
-                    <Chartdropdown title="انتخاب"/>
+                    <Chartdropdown id="0" title="انتخاب" itemList={[{ name: "چیپس", id: "1",selected:false }, { name: "شیرکاکایو", id: "2",selected:false }, { name: "عدسی", id: "3",selected:false }]} func={setFoodsHandler} />
+                    {sat.map((itm,indx)=>{
+                        return (<Box sx={{p:1,textAlign:"right",pr:2,pl:2}}>{itm.name}</Box>)
+                    })}
+                    {sat.length != 0 && <Button onClick={()=>{setSat([])}}>حذف همه</Button>}
                 </Box>
             </Box>
+            <Button sx={{bgcolor:"orange",color:"white",":hover":{bgcolor:"orange"},width:"75px",ml:"auto"}}>تایید</Button>
         </Box>
     )
 
